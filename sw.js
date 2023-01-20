17c6680021494ede45a4ca683b4d886df405923b
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-01-19 21:41:50 -0500

    Fixed an oopsie.

--- sw.js
+++ sw.js
@@ -1,26 +1,16 @@
-// This is just a sample script. Paste your real code (javascript or HTML) here.
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
-} // Check if service workers are supported
+// Check if service workers are supported
 if ('serviceWorker' in navigator) {
-    const registration = await navigator.serviceWorker.ready;
-    // Check if periodicSync is supported
-    if ('periodicSync' in registration) {
-        // Request permission
-        const status = await navigator.permissions.query({
-            name: 'periodic-background-sync',
-        });
-        if (status.state === 'granted') {
-            try {
-                // Register new sync every 24 hours
-                await registration.periodicSync.register('news', {
-                        minInterval: 24 * 60 * 60 * 1000, // 1 day        });        console.log('Periodic background sync registered!');
-                    }
-                    catch (e) {
-                        console.error(`Periodic background sync failed:\n${e}`);
-                    }
-                }
-            }
-        }
+  const registration = await navigator.serviceWorker.ready;
+  // Check if periodicSync is supported
+  if ('periodicSync' in registration) {
+    // Request permission
+    const status = await navigator.permissions.query({      name: 'periodic-background-sync',    });    if (status.state === 'granted') {
+      try {
+        // Register new sync every 24 hours
+        await registration.periodicSync.register('news', {          minInterval: 24 * 60 * 60 * 1000, // 1 day        });        console.log('Periodic background sync registered!');
+      } catch(e) {
+        console.error(`Periodic background sync failed:\n${e}`);
+      }
+    }
+  }
+}
