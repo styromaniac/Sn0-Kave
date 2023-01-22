5fcc75139e7082cdff9d891b3e5a9b63a9c1954d
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-01-22 15:21:00 -0500

    Service worker with background sync every 24 hours

--- sw.js
+++ sw.js
@@ -1,16 +1,73 @@
-// Check if service workers are supported
-if ('serviceWorker' in navigator) {
-  const registration = await navigator.serviceWorker.ready;
-  // Check if periodicSync is supported
-  if ('periodicSync' in registration) {
-    // Request permission
-    const status = await navigator.permissions.query({      name: 'periodic-background-sync',    });    if (status.state === 'granted') {
-      try {
-        // Register new sync every 24 hours
-        await registration.periodicSync.register('news', {          minInterval: 24 * 60 * 60 * 1000, // 1 day        });        console.log('Periodic background sync registered!');
-      } catch(e) {
-        console.error(`Periodic background sync failed:\n${e}`);
+const CACHE_NAME = "my-site-cache-v1";
+const urlsToCache = [
+  '/*'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
-    }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Background Sync
+const syncTag = 'my-site-sync-v1';
+
+self.addEventListener('sync', event => {
+  if (event.tag === syncTag) {
+    event.waitUntil(
+      // Perform background sync here
+      // For example:
+      // fetch('/api/data')
+      //   .then(response => response.json())
+      //   .then(data => {
+      //     // Do something with the data
+      //   })
+    );
   }
-}
+});
+
+// Register background sync every 24 hours
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.registration.sync.register(syncTag)
+    .then(() => {
+      console.log('Background sync registered');
+    })
+    .catch(err => {
+      console.log('Error registering background sync: ', err);
+    })
+  );
+});
+
