9ae84b3de66d37b1bccb7c4942426c35efb42541
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-01-24 16:50:50 -0500

    Add files via upload

--- KaveTorrent.js
+++ KaveTorrent.js
@@ -3,8 +3,9 @@ var script1 = document.createElement("script");
 script1.src = "https://cdn.jsdelivr.net/npm/webtorrent@latest/webtorrent.min.js";
 document.head.appendChild(script1);
 
+// Wait for the script to load
 script1.onload = function() {
-    // Fetch the tracker list
+    // Fetch the tracker list from a provided URL
     fetch('https://kave.styromaniac.com/ClearnetTrackers.txt')
         .then(response => response.text())
         .then(trackerList => {
@@ -12,7 +13,9 @@ script1.onload = function() {
             const client = new WebTorrent({
                 tracker: {
                     ws: trackerList.split('\n'),
-                    http: trackerList.split('\n')
+                    http: trackerList.split('\n'),
+                    // Add additional webseed domains
+                    webseed: ["https://kave.styromaniac.com", "https://kave.loophole.site"]
                 }
             });
 
@@ -20,12 +23,17 @@ script1.onload = function() {
             const elements = document.querySelectorAll("img[src*='Camera/'], video[src*='Camera/']");
             elements.forEach(async element => {
                 try {
+                    // Fetch the resource from the element's src
                     const response = await fetch(element.src);
+                    // Check for a successful response
                     if (!response.ok) {
                         throw new Error(`HTTP error, status = ${response.status}`);
                     }
+                    // Convert the response to a Blob
                     const blob = await response.blob();
+                    // Seed the Blob as a torrent
                     const torrent = client.seed(blob, { name: element.src });
+                    // Log the torrent's name and magnet URI when ready
                     torrent.on('ready', () => {
                         console.log(`Webseeding: ${torrent.name} with magnet URI: ${torrent.magnetURI}`);
                     });
