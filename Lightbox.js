4ec3a4015d708d22dcea256f6fb3f7f5d9a7e2c9
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-01-25 08:24:39 -0500

    Update Lightbox.js

--- Lightbox.js
+++ Lightbox.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
 const lightboxElem = document.getElementById("lightbox");
 const contentElem = document.getElementById("content");
 let player;
@@ -35,3 +36,4 @@ function closeLightbox() {
     contentElem.innerHTML = '';
     lightboxElem.style.display = 'none';
 }
+}, false);
