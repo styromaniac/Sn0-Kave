d44ce6b8980996d8708ae0f1c6f925eace5e1049
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-04 15:12:32 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,60 @@
+const CACHE_NAME = "v1";
+const urlsToCache = ['/'];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.open("site-cache").then(function (cache) {
-      return cache.match(event.request).then(function (response) {
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
         if (response) {
           return response;
         }
 
-        return fetch(event.request).then(function (response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
+        return fetch(event.request).then(
+          function (response) {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== "basic") {
+              return response;
+            }
+
+            // Clone the response. A response is a stream and can only be consumed once.
+            // Since we are consuming this once by cache and once by the browser for fetch,
+            // we need to clone the response
+            let responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
   );
 });
+
+self.addEventListener('sync', function (event) {
+  if (event.tag === '24hr-sync') {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  }
+});
+
+// Schedule the background sync to run every 24 hours
+self.registration.sync.register('24hr-sync');
+setInterval(() => self.registration.sync.register('24hr-sync'), 24 * 60 * 60 * 1000);
