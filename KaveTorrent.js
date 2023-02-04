6d71c1c2be5805d9fe98d724a9055781ea23b123
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-04 17:17:27 -0500

    Update KaveTorrent.js

--- KaveTorrent.js
+++ KaveTorrent.js
@@ -1,46 +1,15 @@
-// Import the WebTorrent library
-var script1 = document.createElement("script");
-script1.src = "https://cdn.jsdelivr.net/npm/webtorrent@latest/webtorrent.min.js";
-document.head.appendChild(script1);
+document.querySelectorAll("img[data-magnet], video[data-magnet]").forEach(function(element) {
+  const magnetURI = element.getAttribute("data-magnet");
+  const client = new WebTorrent();
 
-// Wait for the script to load
-script1.onload = function() {
-    // Fetch the tracker list from a provided URL
-    fetch('https://kave.styromaniac.com/ClearnetTrackers.txt')
-        .then(response => response.text())
-        .then(trackerList => {
-            // Initialize WebTorrent with the tracker list
-            const client = new WebTorrent({
-                tracker: {
-                    ws: trackerList.split('\n'),
-                    http: trackerList.split('\n'),
-                    // Add additional webseed domains
-                    webseed: ["https://kave.styromaniac.com", "https://kave.loophole.site"]
-                }
-            });
+  client.add(magnetURI, function (torrent) {
+    element.src = URL.createObjectURL(torrent.files[0].createReadStream());
 
-            // Get all elements with "Camera" in the src
-            const elements = document.querySelectorAll("img[src*='Camera/'], video[src*='Camera/']");
-            elements.forEach(async element => {
-                try {
-                    // Fetch the resource from the element's src
-                    const response = await fetch(element.src);
-                    // Check for a successful response
-                    if (!response.ok) {
-                        throw new Error(`HTTP error, status = ${response.status}`);
-                    }
-                    // Convert the response to a Blob
-                    const blob = await response.blob();
-                    // Seed the Blob as a torrent
-                    const torrent = client.seed(blob, { name: element.src });
-                    // Log the torrent's name and magnet URI when ready
-                    torrent.on('ready', () => {
-                        console.log(`Webseeding: ${torrent.name} with magnet URI: ${torrent.magnetURI}`);
-                    });
-                } catch (err) {
-                    console.error(err);
-                }
-            });
-        })
-        .catch(error => console.error(error));
-}
+    torrent.on('done', function () {
+      caches.open('torrent-cache')
+        .then(cache => cache.addAll(torrent.files.map(file => file.getBlobURL())))
+        .then(() => client.seed(torrent.files));
+      element.style.outline = "green solid 4px";
+    });
+  });
+});
