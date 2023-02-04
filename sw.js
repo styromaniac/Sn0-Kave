020f6a02fdf3be2a83ac00ce4806ba60c5aeaab8
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-04 16:17:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,17 @@
-const CACHE_NAME = "v1";
-const urlsToCache = ['/'];
-
-self.addEventListener("install", function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        // Cache hit - return response
+    caches.open("site-cache").then(function (cache) {
+      return cache.match(event.request).then(function (response) {
         if (response) {
           return response;
         }
 
-        return fetch(event.request).then(
-          function (response) {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== "basic") {
-              return response;
-            }
-
-            // Clone the response. A response is a stream and can only be consumed once.
-            // Since we are consuming this once by cache and once by the browser for fetch,
-            // we need to clone the response
-            let responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function (cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
+        return fetch(event.request).then(function (response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
   );
 });
 
