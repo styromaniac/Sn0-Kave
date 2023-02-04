b387480d0cdba526964126760da4d749ed6c79ad
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-03 23:30:42 -0500

    Update Copyright.js

--- Copyright.js
+++ Copyright.js
@@ -1,11 +1,14 @@
-  var copyright = document.querySelector(".copyright");
-  var mit = document.querySelector("#MIT");
+var copyright = document.querySelector(".copyright");
+var mit = document.querySelector("#MIT");
 
-  copyright.addEventListener("click", function() {
-    mit.classList.toggle("visible");
-    mit.addEventListener("transitionend", function() {
-      setTimeout(function() {
-        window.scrollTo(0, document.body.scrollHeight);
-      }, 1001);
-    });
+copyright.addEventListener("click", function() {
+  mit.classList.toggle("visible");
+
+  var observer = new MutationObserver(function() {
+    if (mit.style.height === "100%") {
+      window.scrollTo(0, document.body.scrollHeight);
+      observer.disconnect();
+    }
   });
+  observer.observe(mit, { attributes: true });
+});
