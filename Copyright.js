230e388f1f0c288da5cf37068997a5e1314a0885
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-03 23:24:03 -0500

    Update Copyright.js

--- Copyright.js
+++ Copyright.js
@@ -6,6 +6,6 @@
     mit.addEventListener("transitionend", function() {
       setTimeout(function() {
         window.scrollTo(0, document.body.scrollHeight);
-      }, 100);
+      }, 1000);
     });
   });
