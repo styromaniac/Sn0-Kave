ab0b3dd151f40bd0a60004b1cbc724b03521ff59
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-06 01:39:48 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,69 @@
-// Listen for installation event
-self.addEventListener("install", function(event) {
-  console.log("Service worker installed");
+// sw.js
+
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  'https://cdn.jsdelivr.net/npm/webtorrent@latest/webtorrent.min.js',
+  '/index.html',
+  '/*'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Listen for activation event
-self.addEventListener("activate", function(event) {
-  console.log("Service worker activated");
+self.addEventListener('activate', function(event) {
+  console.log('Activating new service worker...');
+
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
-// Listen for fetch events
-self.addEventListener("fetch", function(event) {
-  console.log("Service worker fetching resource: " + event.request.url);
-});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-// Listen for sync event
-self.addEventListener("sync", function(event) {
-  console.log("Service worker syncing data");
-  if (event.tag === "background-sync") {
-    // Perform background sync here
-    // Add the code from the script
-    document.querySelectorAll("img[data-magnet], video[data-magnet]").forEach(function(element) {
-      const magnetURI = element.getAttribute("data-magnet");
-      const client = new WebTorrent();
-  
-      client.add(magnetURI, function (torrent) {
-        element.src = URL.createObjectURL(torrent.files[0].createReadStream());
-  
-        torrent.on('done', function () {
-          client.seed(torrent.files);
-        });
-      });
-    });
-  }
-});
+        var fetchRequest = event.request.clone();
 
-// Register the service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker.register("/sw.js").then(function(registration) {
-      console.log("Service worker registration successful: ", registration.scope);
-    }, function(error) {
-      console.log("Service worker registration failed: ", error);
-    });
-  });
-}
-setInterval(function() {
-  console.log("Triggering background sync");
-  self.registration.sync.register("background-sync");
-}, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
