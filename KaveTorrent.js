1f0d284cd3365eb8d8a6265ac0869ed890eab0f5
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-06 21:20:28 -0500

    Update KaveTorrent.js

--- KaveTorrent.js
+++ KaveTorrent.js
@@ -2,13 +2,19 @@ document.querySelectorAll("img[data-magnet], video[data-magnet]").forEach(functi
   const magnetURI = element.getAttribute("data-magnet");
   const client = new WebTorrent();
 
-  client.add(magnetURI, function (torrent) {
-    element.src = URL.createObjectURL(torrent.files[0].createReadStream());
+  caches.match(element.src).then(function(response) {
+    if (response) {
+      element.src = response.url;
+    } else {
+      client.add(magnetURI, function (torrent) {
+        element.src = URL.createObjectURL(torrent.files[0].createReadStream());
 
-    torrent.on('done', function () {
-      caches.open('torrent-cache')
-        .then(cache => cache.addAll(torrent.files.map(file => file.getBlobURL())))
-        .then(() => client.seed(torrent.files));
-    });
+        torrent.on('done', function () {
+          caches.open('torrent-cache')
+            .then(cache => cache.addAll(torrent.files.map(file => file.getBlobURL())))
+            .then(() => client.seed(torrent.files));
+        });
+      });
+    }
   });
 });
