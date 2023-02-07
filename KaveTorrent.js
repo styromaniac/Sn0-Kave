ccace57aa7106f0fb9660055af05d55337d367ca
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-06 21:33:54 -0500

    Update KaveTorrent.js

--- KaveTorrent.js
+++ KaveTorrent.js
@@ -2,19 +2,36 @@ document.querySelectorAll("img[data-magnet], video[data-magnet]").forEach(functi
   const magnetURI = element.getAttribute("data-magnet");
   const client = new WebTorrent();
 
-  caches.match(element.src).then(function(response) {
-    if (response) {
-      element.src = response.url;
-    } else {
-      client.add(magnetURI, function (torrent) {
-        element.src = URL.createObjectURL(torrent.files[0].createReadStream());
+  const dbName = 'torrent-db';
+  const objectStoreName = 'torrent-files';
 
-        torrent.on('done', function () {
-          caches.open('torrent-cache')
-            .then(cache => cache.addAll(torrent.files.map(file => file.getBlobURL())))
-            .then(() => client.seed(torrent.files));
+  var request = indexedDB.open(dbName);
+  request.onsuccess = function(event) {
+    var db = event.target.result;
+    var objectStore = db.transaction([objectStoreName], 'readonly').objectStore(objectStoreName);
+    var fileRequest = objectStore.get(element.src);
+    fileRequest.onsuccess = function(event) {
+      if (fileRequest.result) {
+        element.src = URL.createObjectURL(fileRequest.result);
+      } else {
+        client.add(magnetURI, function (torrent) {
+          element.src = URL.createObjectURL(torrent.files[0].createReadStream());
+
+          torrent.on('done', function () {
+            var db = event.target.result;
+            var transaction = db.transaction([objectStoreName], 'readwrite');
+            var objectStore = transaction.objectStore(objectStoreName);
+
+            torrent.files.forEach(function(file) {
+              objectStore.put(file.getBlob(), file.name);
+            });
+
+            transaction.oncomplete = function() {
+              client.seed(torrent.files);
+            };
+          });
         });
-      });
-    }
-  });
+      }
+    };
+  };
 });
