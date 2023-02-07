d2797e301c28a83ed406bbc64c35673d060dc8fd
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-06 22:43:38 -0500

    Update KaveTorrent.js

--- KaveTorrent.js
+++ KaveTorrent.js
@@ -1,6 +1,6 @@
-document.querySelectorAll("img[data-magnet], video[data-magnet]").forEach(function(element) {
+document.querySelectorAll("img[data-magnet][src$='.jpg'], video[data-magnet][src$='.mp4']").forEach(function(element) {
   const magnetURI = element.getAttribute("data-magnet");
-  const client = new WebTorrent();
+  const client = new WebTorrent({dht: true});
 
   const dbName = 'torrent-db';
   const objectStoreName = 'torrent-files';
@@ -12,24 +12,52 @@ document.querySelectorAll("img[data-magnet], video[data-magnet]").forEach(functi
     var fileRequest = objectStore.get(element.src);
     fileRequest.onsuccess = function(event) {
       if (fileRequest.result) {
-        element.src = URL.createObjectURL(fileRequest.result);
+        var cachedFile = fileRequest.result;
+        client.add(magnetURI, {
+          path: cachedFile.webkitRelativePath,
+          file: cachedFile
+        }, function (torrent) {
+          var file = torrent.files.find(function(file) {
+            return file.name.endsWith('.jpg') || file.name.endsWith('.mp4');
+          });
+
+          if (file) {
+            element.src = URL.createObjectURL(file.createReadStream());
+
+            torrent.on('done', function () {
+              var db = event.target.result;
+              var transaction = db.transaction([objectStoreName], 'readwrite');
+              var objectStore = transaction.objectStore(objectStoreName);
+
+              objectStore.put(file.getBlob(), file.name);
+
+              transaction.oncomplete = function() {
+                client.seed(torrent.files);
+              };
+            });
+          }
+        });
       } else {
         client.add(magnetURI, function (torrent) {
-          element.src = URL.createObjectURL(torrent.files[0].createReadStream());
-
-          torrent.on('done', function () {
-            var db = event.target.result;
-            var transaction = db.transaction([objectStoreName], 'readwrite');
-            var objectStore = transaction.objectStore(objectStoreName);
-
-            torrent.files.forEach(function(file) {
-              objectStore.put(file.getBlob(), file.name);
-            });
-
-            transaction.oncomplete = function() {
-              client.seed(torrent.files);
-            };
+          var file = torrent.files.find(function(file) {
+            return file.name.endsWith('.jpg') || file.name.endsWith('.mp4');
           });
+
+          if (file) {
+            element.src = URL.createObjectURL(file.createReadStream());
+
+            torrent.on('done', function () {
+              var db = event.target.result;
+              var transaction = db.transaction([objectStoreName], 'readwrite');
+              var objectStore = transaction.objectStore(objectStoreName);
+
+              objectStore.put(file.getBlob(), file.name);
+
+              transaction.oncomplete = function() {
+                client.seed(torrent.files);
+              };
+            });
+          }
         });
       }
     };
