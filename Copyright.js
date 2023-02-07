a357bd5858a484d8b10c46448e0d80a52847d281
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-07 00:13:34 -0500

    Delete Copyright.js

--- Copyright.js
+++
@@ -1,6 +0,0 @@
-  var copyright = document.querySelector(".copyright");
-  var mit = document.querySelector("#MIT");
-
-  copyright.addEventListener("click", function() {
-    mit.classList.toggle("visible");
-  });
