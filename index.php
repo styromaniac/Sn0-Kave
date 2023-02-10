ddadd2562fee4b1cb56e7fd5cc76d2f6926d508a
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2023-02-10 14:21:59 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                 	<?php echo "Last synced: " . date ("F d Y H:i:s", getlastmod()); ?>  UTC.
                     </span>
                 	<br>
-                	<span class="copyright">
+                	<div class="copyright">
                 	<?php echo "Kave &copy 2018-".date ("Y", getlastmod()); ?> Alex "Styromaniac" Goven
                     <br>
                     <img src="dep/MIT.svg">
@@ -82,7 +82,7 @@ The above copyright notice and this permission notice shall be included in all c
 <br>
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 </div>
-                	</span>
+                	</div>
                 <div id="lightbox">
                     <div class="bar">
                         <a oncontextmenu="toggleFullScreen()" onclick="window.closeLightbox()">
